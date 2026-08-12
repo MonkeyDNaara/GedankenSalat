51f0947dfbb140aa74730cfaa277b4f9570cc138
Niko <naara21@hotmail.de>
2026-08-12 19:23:06 +0200

    header content and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="navbar bg-primary flex justify-between">
+        <h1 className="text-2xl text-primary-content">GedankenSalat</h1>
+        <button className="btn btn-secondary">+ Add entry</button>
+      </div>
+    </header>
+  );
 };
 export default Header;
