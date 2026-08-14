1abf286d2c77a81f4a84e7508dc73d78e42c8d35
Niko <naara21@hotmail.de>
2026-08-14 16:31:28 +0200

    bug resolving darkmode header color

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -7,9 +7,7 @@ const EntryList = () => {
 
   return (
     <div className="bg-base-200 mb-auto">
-      <h2 className="text-2xl text-accent-content p-4">
-        Entries - newest first
-      </h2>
+      <h2 className="text-2xl p-4">Entries - newest first</h2>
       <div className="px-8 pb-8 grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4">
         {entries.map((entry) => (
           <EntryCard entry={entry} key={entry.id} />
